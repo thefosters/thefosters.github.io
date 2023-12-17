650ed5bf2e23c7aa71958e6b865d78ba1b376c63
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2023-12-17 15:23:07 +0000

    cool background update

--- js/coolbackground.js
+++ js/coolbackground.js
@@ -7,4 +7,36 @@ function setRandomBackgroundColor() {
   document.querySelector('.background').style.backgroundColor = generateRandomColor();
 }
 
-setInterval(setRandomBackgroundColor, 1000);
+function darkmode(){
+  window.sessionStorage.coolbackground = "no";
+  document.querySelector('.background').style.backgroundColor = "#000";
+  var darkb = document.getElementById("dark");
+  var lightb = document.getElementById("light");
+  darkb.classList.add("hiden");
+  lightb.classList.remove("hiden");
+  clearInterval(coolbackground2)
+}
+
+function lightmode(){
+  window.sessionStorage.coolbackground = "yes";
+  document.querySelector('.background').style.backgroundColor = "#a561f5";
+  var darkb = document.getElementById("dark");
+  var lightb = document.getElementById("light");
+  darkb.classList.remove("hiden");
+  lightb.classList.add("hiden");
+  coolbackground2 = setInterval(setRandomBackgroundColor, 1000);
+}
+
+let coolbackground = sessionStorage.getItem("coolbackground");
+console.log(coolbackground)
+if (coolbackground != "no"){
+  coolbackground2 = setInterval(setRandomBackgroundColor, 1000);
+}
+
+if(coolbackground == "no"){
+  document.querySelector('.background').style.backgroundColor = "#000";
+  var darkb = document.getElementById("dark");
+  var lightb = document.getElementById("light");
+  darkb.classList.add("hiden");
+  lightb.classList.remove("hiden");
+}
