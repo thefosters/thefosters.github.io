2c56dadbf54fc28fc776ced55314958c0fc48489
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2023-12-18 16:36:23 +0000

    update

--- daniel/cownt_down_tim/script.js
+++ daniel/cownt_down_tim/script.js
@@ -1,4 +1,4 @@
-var targetDate = new Date("December 25, 2023 00:00:00").getTime();
+var chrismas_c = new Date("December 25, 2023 00:00:00").getTime();
 
 var countdownInterval = setInterval(updateCountdown, 1000);
 
@@ -9,90 +9,6 @@ function updateCountdown() {
     if (timeDifference <= 0) {
         clearInterval(countdownInterval);
         document.getElementById("chrismas_c").innerHTML = "Countdown expired!";
-    } else {
-        var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-
-        document.getElementById("countdown").innerHTML = `${days}days ${hours}hours ${minutes}minutes ${seconds}seconds`;
-    }
-}
-
-var targetDate = new Date("October 22, 2024 00:00:00").getTime();
-
-var countdownInterval = setInterval(updateCountdown, 1000);
-
-function updateCountdown() {
-    var currentDate = new Date().getTime();
-    var timeDifference = targetDate - currentDate;
-
-    if (timeDifference <= 0) {
-        clearInterval(countdownInterval);
-        document.getElementById("nathan_c").innerHTML = "Countdown expired!";
-    } else {
-        var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-
-        document.getElementById("countdown").innerHTML = `${days}days ${hours}hours ${minutes}minutes ${seconds}seconds`;
-    }
-}
-
-var targetDate = new Date("February 4, 2024 00:00:00").getTime();
-
-var countdownInterval = setInterval(updateCountdown, 1000);
-
-function updateCountdown() {
-    var currentDate = new Date().getTime();
-    var timeDifference = targetDate - currentDate;
-
-    if (timeDifference <= 0) {
-        clearInterval(countdownInterval);
-        document.getElementById("jo_c").innerHTML = "Countdown expired!";
-    } else {
-        var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-
-        document.getElementById("countdown").innerHTML = `${days}days ${hours}hours ${minutes}minutes ${seconds}seconds`;
-    }
-}
-
-var targetDate = new Date("April 19, 2024 00:00:00").getTime();
-
-var countdownInterval = setInterval(updateCountdown, 1000);
-
-function updateCountdown() {
-    var currentDate = new Date().getTime();
-    var timeDifference = targetDate - currentDate;
-
-    if (timeDifference <= 0) {
-        clearInterval(countdownInterval);
-        document.getElementById("daniel_c").innerHTML = "Countdown expired!";
-    } else {
-        var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-
-        document.getElementById("countdown").innerHTML = `${days}days ${hours}hours ${minutes}minutes ${seconds}seconds`;
-    }
-}
-
-var targetDate = new Date("November 16, 2024 00:00:00").getTime();
-
-var countdownInterval = setInterval(updateCountdown, 1000);
-
-function updateCountdown() {
-    var currentDate = new Date().getTime();
-    var timeDifference = targetDate - currentDate;
-
-    if (timeDifference <= 0) {
-        clearInterval(countdownInterval);
-        document.getElementById("howard_c").innerHTML = "Countdown expired!";
     } else {
         var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
         var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
