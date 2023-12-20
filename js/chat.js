7b1505e0832d00851ee430a5bd2fa755909fdfc8
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2023-12-20 18:40:53 +0000

    adding more chat functions

--- js/chat.js
+++ js/chat.js
@@ -6,16 +6,53 @@ var chatInput = document.getElementById("chatInput");
 chatHeader.addEventListener("click", toggleChat);
 chatInput.addEventListener("keydown", sendMessage);
 
+
+
+
+
+
+function updateCountdown() {
+    var chrismas_c = new Date("December 25, 2023 00:00:00").getTime();
+    var currentDate = new Date().getTime();
+    var timeDifference = chrismas_c - currentDate;
+
+    if (timeDifference <= 0) {
+        clearInterval(countdownInterval);
+        appendMessage("Tim:", "Sorry i do not know" );
+    } else {
+        var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        appendMessage("Tim:", "it is in: " + `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`);
+    }
+}
+
+
+
+
+
+
 function chech_do(you) {
   if(you == "hi" || you == "hello"){
     appendMessage("Tim:", "Hi, how can i help");
 
-  }else if(you == "daniel" || you == "nathan"){
+  }else if(you == "daniel" || you == "nathan" || you == "jo" || you == "howard"){
     appendMessage("Tim:", "Redirecting you to " + you);
     window.location.href = "/"+you + "/index.html"
+
+  }else if(you == "how long till christmas"){
+    updateCountdown()
+
+  }else if(you == "bye"){
+    appendMessage("Tim:", "Bye 👋");
+    await delay(100);
+    location.reload()
   }
   
 }
+
+
 
 
 function toggleChat() {
