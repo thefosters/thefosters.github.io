7c9817d3def5dca60b1d31ea3e68c6fc095be2f0
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2023-12-29 15:49:24 +0000

    Update script.js

--- daniel/cownt_down_tim/script.js
+++ daniel/cownt_down_tim/script.js
@@ -1,4 +1,4 @@
-var chrismas_c = new Date("December 25, 2023 00:00:00").getTime();
+var chrismas_c = new Date("December 25, 2024 00:00:00").getTime();
 
 
 var countdownInterval = setInterval(updateCountdown, 1000);
